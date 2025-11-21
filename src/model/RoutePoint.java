528587b5dc7865fd3a6521de1a94d075f942f6f5
Shakhpazy <shakhpazyusuf@gmail.com>
2025-11-20 16:22:24 -0800

    Java Doc on RoutePoint

--- src/model/RoutePoint.java
+++ src/model/RoutePoint.java
@@ -2,26 +2,45 @@ package model;
 
 public class RoutePoint {
 
+    /** The altitude of the route point */
     private final float myAltitude;
 
+    /** the longitude of the route point */
     private final float myLongitude;
 
+    /** the latitude of the route point */
     private final float myLatitude;
 
+    /**
+     * Constructor for RoutePoint
+     *
+     * @param theLongitude the longitude
+     * @param theLatitude the latitude
+     * @param theAltitude the altitude
+     */
     public RoutePoint(float theLongitude, float theLatitude, float theAltitude) {
         myLongitude = theLongitude;
         myLatitude = theLatitude;
         myAltitude = theAltitude;
     }
 
+    /**
+     * @return the altitude of the route point
+     */
     public float getAltitude() {
         return myAltitude;
     }
 
+    /**
+     * @return the longitude of the route point
+     */
     public float getLongitude() {
         return myLongitude;
     }
 
+    /**
+     * @return the latitude of the route point
+     */
     public float getLatitude() {
         return myLatitude;
     }
