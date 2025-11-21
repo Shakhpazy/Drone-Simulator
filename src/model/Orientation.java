5eae20b2c1e442c74808f2bc72e831b433128094
Shakhpazy <shakhpazyusuf@gmail.com>
2025-11-20 16:10:39 -0800

    Java Doc on the Orientation

--- src/model/Orientation.java
+++ src/model/Orientation.java
@@ -1,30 +1,51 @@
 package model;
 
 
-//Currently the Degree 0 should be east with this current implementation
 public class Orientation {
 
+    /** The current degree of the Drone */
     float myDegree;
 
+    /**
+     * Creates an orientation object
+     *
+     * @param theDegree starting degree
+     */
     public Orientation(float theDegree) {
         myDegree = theDegree;
     }
 
+    /**
+     * @return {float} of the degree
+     */
     public float getDegree() {
         return myDegree;
     }
 
+    /**
+     * Sets the degree of the drone, also is circular
+     * so the degree range will always be between 0-359
+     *
+     * @param theDegrees {float} of the degree the drone is facing
+     */
     public void setDegrees(float theDegrees) {
         // Normalize using modulo arithmetic
         myDegree = ((theDegrees % 360) + 360) % 360;
     }
 
-    /** Returns the difference (in degrees) between this orientation and another. */
+    /**
+     *
+     * @param thePrevLong the previous longitude of the Drone
+     * @param thePrevLat the previous latitude of the Drone
+     * @param theNextLong the next longitude of the Drone
+     * @param theNextLat the next latitude of the Drone
+     *
+     * @return {float} degree of the next orientation
+     */
     public float findNextOrientation(float thePrevLong, float thePrevLat, float theNextLong, float theNextLat) {
         float dx = theNextLong - thePrevLong;
         float dy = theNextLat - thePrevLat;
 
-        // Default atan2 gives 0° = East, so rotate 90° counterclockwise to make 0° = North
         float angleDegrees = (float) Math.toDegrees(Math.atan2(dx, dy));
 
         // Normalize to 0–360 range
